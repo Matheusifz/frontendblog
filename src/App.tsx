6184823509a269d1fe667b11694ca74ec4ec2129
matheusifz <matheusifz@gmail.com>
2021-10-06 18:26:39 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,16 @@ import { useState } from "react";
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Route path="/" exact>
+          <button>Login</button>
+          <button>Logout</button>
+        </Route>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
