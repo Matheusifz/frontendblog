112137d58a4c9e7bb68d8e164392804e7c7c0e0c
matheusifz <matheusifz@gmail.com>
2021-10-06 16:12:46 -0300

    chore: add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
