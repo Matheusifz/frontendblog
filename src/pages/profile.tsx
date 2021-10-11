c8c27797bcb38c190600e7c1ac55d8ddd064d352
matheusifz <matheusifz@gmail.com>
2021-10-11 16:28:21 -0300

    feat: create profile component

---
+++ src/pages/profile.tsx
@@ -0,0 +1,13 @@
+import { withRouter } from "react-router-dom";
+
+interface Profile {
+}
+
+const Profile: React.FC<Profile> = ({
+}) => {
+  return (
+    <div>If you are seeing this you are now authenticated.</div>
+  )
+}
+
+export default withRouter(Profile);
