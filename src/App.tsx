f00ccd218e7bda3fc087f5a34667abf9c3c4fcc3
matheusifz <matheusifz@gmail.com>
2021-10-11 18:57:02 -0300

    feat: create and integrate protected route

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import ProtectedRoute from "./ProtectedRoute";
+import Profile from "./pages/profile";
 
 function App() {
-  return <div className="App"></div>;
+  const [isAuth, setIsAuth] = useState(false);
+  return (
+    <div className="App">
+      <Router>
+        <Route path="/" exact>
+          <button>Login</button>
+          <button>Logout</button>
+        </Route>
+        <ProtectedRoute
+          path="/profile"
+          component={Profile}
+          children
+          isAuth={isAuth}
+        />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/ProtectedRoute.tsx
+++ src/ProtectedRoute.tsx
@@ -1,30 +1,55 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect, useLocation } from "react-router-dom";
 
 interface ProtectedRoute {
   isAuth: any;
   children: any;
+  path: string;
+  component: any;
 }
 
-const ProtectedRoute: React.FC<ProtectedRoute> = ({
+/* const ProtectedRoute: React.FC<ProtectedRoute> = ({
   isAuth,
   children,
   ...rest
 }) => {
+  console.log('isAuth, props');
+  console.log(isAuth, props);
   return (
     <Route
       {...rest}
       render={(props) => {
         if (isAuth) {
-          return <> {children}</>;
+          
         } else {
+          console.log(isAuth, props);
           return (
             <Redirect to={{ pathname: "/", state: { from: props.location } }} />
           );
         }
       }}
     />
-  );
+  ); 
+};*/
+
+const ProtectedRoute: React.FC<ProtectedRoute> = ({
+  isAuth,
+  children,
+  ...rest
+}) => {
+  const location = useLocation();
+
+  const returnRoute = () => {
+    if (isAuth) {
+      return <> {children}</>;
+    } else {
+      return (
+        <Redirect to={{ pathname: "/", state: { from: location.pathname } }} />
+      );
+    }
+  };
+
+  return returnRoute();
 };
 
 export default ProtectedRoute;
